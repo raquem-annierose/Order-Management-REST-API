925de4233d7edee3df1f20f8c639e3aea0101a6c
MikeeCapilitan <mikee.capilitan@gmail.com>
2026-06-13 17:26:42 +0800

    fix order api

--- src/main/java/com/pup/taguig/ordermanagement/dto/OrderResponseDTO.java
+++ src/main/java/com/pup/taguig/ordermanagement/dto/OrderResponseDTO.java
@@ -2,17 +2,17 @@ package com.pup.taguig.ordermanagement.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
+import java.util.List;
 @Getter
 @Setter
 public class OrderResponseDTO {
-
     private Long id;
     private Long customerId;
     private String status;
     private BigDecimal totalPrice;
     private LocalDateTime createdAt;
+    
+    private List<OrderItemResponseDTO> items; 
 }
